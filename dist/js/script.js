17a900fae56865457cda0c6cbf0bbb8a5c38fbdb
Nick <sytnyk.m@ukr.net>
2021-04-06 21:41:55 +0300

    with fixed bugs and counters script

--- dist/js/script.js
+++ dist/js/script.js
@@ -10,3 +10,10 @@ closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
 
+
+const counters = document.querySelectorAll('.percent__name-pe'),
+      lines = document.querySelectorAll('.percent__gradation-plus');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
